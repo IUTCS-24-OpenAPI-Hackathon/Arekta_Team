4a338b97af734d6a19bb12cd5b33577483cd0f74
Md Alamin <mdalamincodes@gmail.com>
2024-04-27 02:14:33 +0600

    feat(client): add map

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
   return (
-    <div className="py-3"><Outlet/></div>
+    <div className=""><Outlet/></div>
   )
 }
 
--- client/src/pages/GetUserLocation/GetUserLocation.jsx
+++ client/src/pages/GetUserLocation/GetUserLocation.jsx
@@ -1,19 +1,30 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-import Container from '@/components/ui/shared/Container'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import Map, {
+  Marker,
+  Popup,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl,
+} from 'react-map-gl'
 import { toast } from 'sonner'
-import NearestData from './NearestData'
+import Pin from './Pin'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
+const TOKEN =
+  'pk.eyJ1IjoiYWxhbWluODczMyIsImEiOiJjbHZndnd5MHYwdmllMmtwNTBjMDA4Ymk2In0.QVTrogk-akJwfVR2rHEV-g'
 const GetUserLocation = () => {
   const [location, setLocation] = useState({
     latitude: '',
     longitude: '',
     range: 1000,
-  });
-  const [nearestAres, setNearestAreas] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
- 
+  })
+  const [nearestAres, setNearestAreas] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [popupInfo, setPopupInfo] = useState(null)
+  console.log(popupInfo)
   // get user geo location
   const getUserLocation = () => {
     if (navigator.geolocation) {
@@ -65,41 +76,81 @@ const GetUserLocation = () => {
       longitude: longitude,
       range: range,
     }))
-    
   }
 
   const getNearestData = async () => {
     try {
-      setIsLoading(true);
+      setIsLoading(true)
       const res = await fetch(
         `http://localhost:2600/api/v1/geo/poi?latitude=${location.latitude}&longitude=${location.longitude}&range=${location.range}`,
       )
-      const data = await res.json();
-      setNearestAreas(data.result.elements);
-      setIsLoading(false);
+      const data = await res.json()
+      setNearestAreas(data.result.elements)
+      setIsLoading(false)
     } catch (error) {
-      setIsLoading(false);
+      setIsLoading(false)
       console.log(error.message)
     }
   }
+
+  const pins = useMemo(
+    () =>
+      nearestAres.map((city, index) => {
+        return (
+          <Marker
+            key={`marker-${index}`}
+            longitude={city.lon}
+            latitude={city.lat}
+            anchor='bottom'
+            onClick={e => {
+              // If we let the click event propagates to the map, it will immediately close the popup
+              // with `closeOnClick: true`
+              e.originalEvent.stopPropagation()
+              setPopupInfo(city)
+            }}
+          >
+            <Pin />
+          </Marker>
+        )
+      }),
+    [nearestAres],
+  )
+  // console.log(pin)
   useEffect(() => {
     if (location.latitude) {
       getNearestData()
     }
   }, [location.latitude])
   return (
-    <section className='py-5'>
-      <Container className='flex flex-col items-center justify-center'>
-        <div className='flex flex-col max-w-[500px]'>
+    <section className=''>
+      <div className='grid grid-cols-12 gap-5 lg:flex-row'>
+        <div className='flex flex-col w-full col-span-4 m-2'>
           <div>
             <form
               onSubmit={handleSubmitForm}
               className='flex items-center gap-2'
             >
-              <Input defaultValue={location.latitude} type='text' placeholder='Latitude' name='latitude' />
-              <Input defaultValue={location.longitude} type='text' placeholder='Longitude' name='longitude' />
-              <Input defaultValue={location.range} type='text' placeholder='Range' name='range' />
-              <Button type='submit'>{isLoading ? "Searching...": "Search"}</Button>
+              <Input
+                defaultValue={location.latitude}
+                type='text'
+                placeholder='Latitude'
+                name='latitude'
+              />
+              <Input
+                defaultValue={location.longitude}
+                type='text'
+                placeholder='Longitude'
+                name='longitude'
+              />
+              <Input
+                defaultValue={location.range}
+                type='text'
+                placeholder='Range'
+                name='range'
+              />
+              <Button type='submit'>
+                {isLoading ? 'Searching...' : 'Search'}
+              </Button>
             </form>
           </div>
 
@@ -111,12 +162,49 @@ const GetUserLocation = () => {
         </div>
         {/* nearest data */}
 
-        <div className='w-full max-w-[500px] flex-col flex mt-5'>
-          {nearestAres.map((nearestArea, index) => (
-            <NearestData key={index} nearestArea={nearestArea} />
-          ))}
+        <div className='col-span-8'>
+          <Map
+            mapLib={import('mapbox-gl')}
+            initialViewState={{
+              latitude: 25.1634,
+              longitude: 92.0175,
+              zoom: 7,
+              bearing: 0,
+              pitch: 0,
+            }}
+            mapStyle='mapbox://styles/mapbox/dark-v9'
+            style={{ width: '100%', height: '100vh' }}
+            mapboxAccessToken={TOKEN}
+          >
+            <GeolocateControl position='top-left' />
+            <FullscreenControl position='top-left' />
+            <NavigationControl position='top-left' />
+            <ScaleControl />
+
+            {pins}
+
+            {popupInfo && (
+              <Popup
+                anchor='top'
+                longitude={Number(popupInfo.longitude)}
+                latitude={Number(popupInfo.latitude)}
+                onClose={() => setPopupInfo(null)}
+              >
+                <div>
+                  {popupInfo.city}, {popupInfo.state} |{' '}
+                  <a
+                    target='_new'
+                    href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.city}, ${popupInfo.state}`}
+                  >
+                    Wikipedia
+                  </a>
+                </div>
+                <img width='100%' src={popupInfo.image} />
+              </Popup>
+            )}
+          </Map>
         </div>
-      </Container>
+      </div>
     </section>
   )
 }
