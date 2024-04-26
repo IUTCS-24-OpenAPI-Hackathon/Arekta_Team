1f7b376542b7a8a72166cf648a5ad46f0f099263
SrejonKhan <srejonkhan9@gmail.com>
2024-04-26 16:54:47 +0600

    feat(server): update get poi's query

--- server/src/controllers/geo.controller.ts
+++ server/src/controllers/geo.controller.ts
@@ -1,44 +1,44 @@
 import { NextFunction, Request, Response } from 'express';
 
 const handleGetPointOfInterest = async (
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) => {
-    try {
-        const latitude = req.query.latitude;
-        const longitude = req.query.longitude;
-        const range = req.query.range;
+  try {
+    const latitude = req.query.latitude;
+    const longitude = req.query.longitude;
+    const range = req.query.range;
 
-        const overpassReq = await fetch(
-            "https://overpass-api.de/api/interpreter",
-            {
-                method: "POST",
-                body: "data=" + encodeURIComponent(`
+    const overpassReq = await fetch('https://overpass-api.de/api/interpreter', {
+      method: 'POST',
+      body:
+        'data=' +
+        encodeURIComponent(`
                     [out:json]
                     [timeout:90]
                     ;
                     (
-                        node["amenity"](around:${range},${latitude},${longitude});
+                        node["tourism"](around:${range},${latitude},${longitude});
+                        node["place"](around:${range},${latitude},${longitude});
                     );
                     out geom;
-                `)
-            },
-        )
+                `),
+    });
 
-        const result = await overpassReq.json();
+    const result = await overpassReq.json();
 
-        res.status(200).json({
-            success: true,
-            message: 'Successfully retrieved POI around the lat long!',
-            result
-        });
-    } catch (error) {
-        next(error);
-    }
+    res.status(200).json({
+      success: true,
+      message: 'Successfully retrieved POI around the lat long!',
+      result,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const geoControllers = {
-    handleGetPointOfInterest,
+  handleGetPointOfInterest,
 };
 export default geoControllers;
