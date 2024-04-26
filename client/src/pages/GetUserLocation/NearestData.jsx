7e75ef7e6cf521c7f7f320335321140979a745c1
Md Alamin <mdalamincodes@gmail.com>
2024-04-26 18:16:21 +0600

    fix(client): fic nearest card

--- client/src/pages/GetUserLocation/NearestData.jsx
+++ client/src/pages/GetUserLocation/NearestData.jsx
@@ -3,10 +3,9 @@
 const NearestData = ({nearestArea}) => {
   return (
     <div className="relative flex items-center justify-center w-full p-5 mt-2 border shadow">
-       {nearestArea.tags.place &&  <div className="absolute flex items-center justify-center px-2 bg-gray-300 rounded-full top-1 right-1"><p>
-       { nearestArea.tags.place }</p></div>}
-       {/* {nearestArea.tags?.name ? <h3 className="text-2xl font-bold">{nearestArea.tags?.name}</h3>: <h3 className="text-2xl font-bold">{nearestArea.tags.name:en}</h3>} */}
-       <h3 className="text-2xl font-bold">{nearestArea.tags?.name}</h3>
+       {nearestArea.tags.amenity &&  <div className="absolute flex items-center justify-center px-2 bg-gray-300 rounded-full top-1 right-1"><p>
+       { nearestArea.tags.amenity }</p></div>}
+       <h3 className="text-2xl font-bold text-center">{nearestArea.tags?.name}</h3>
         
     </div>
   )
