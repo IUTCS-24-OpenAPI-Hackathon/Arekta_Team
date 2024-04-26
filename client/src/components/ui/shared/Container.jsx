ed2ab13cc8a4d55c72e81421c6dcd81245a10756
Md Alamin <mdalamincodes@gmail.com>
2024-04-26 13:00:08 +0600

    client init

---
+++ client/src/components/ui/shared/Container.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+
+const Container = ({children}) => {
+  return (
+    <div className="w-full px-2 mx-auto max-w-7xl lg:px-0">{children}</div>
+  )
+}
+
+export default Container
