681fcd2fd5654afbc4c3011f26e83235512b9d4a
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2024-04-27 09:11:45 +0600

    feat(server): implement user sign in service function

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -12,7 +12,16 @@ const createNewUserIntoDB = async (
   return user;
 };
 
+const signInUserIntoDB=async(email:string,password:string)=>{
+  const user=await User.findOne({
+    email,
+    password
+  })
+  return user;
+}
+
 const userServices = {
   createNewUserIntoDB,
+  signInUserIntoDB
 };
 export default userServices;
